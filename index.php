6516485f61fb28ab5868cdd8c90c2e80eaee3e13
Red-Beard7 <michael.nabangi@strathmore.edu>
2020-12-11 05:05:48 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,274 @@
+<!doctype html>
+<html lang="en-gb">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pillars of Hope</title>
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
+</head>
+<body>
+<div id="navigation-sticky">
+
+    <!-- Navigation -->
+    <nav class="navbar bg-secondary navbar-light navbar-expand-lg sticky-top">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">
+                <img src="img/pillars%20logo.png" alt="logo" title="home logo">Pillars
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_responsive">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbar_responsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Gallery</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">About</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Services</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navigation -->
+
+    <!-- Image Carousel -->
+    <div class="carousel slide" id="carousel" data-ride="carousel" data-interval="900000">
+
+        <!-- Carousel Content -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="w-100" src="img/children/4.jpg" alt="">
+                <div class="carousel-caption">
+                    <div class="container">
+                        <div class="row justify-content-end">
+                            <div class="d-none d-md-block col-md-11 col-lg-9 col-xl-8 bg-dark text-right">
+                                <h3 class="pb-3">Pillars of Hope Children's Home</h3>
+                                <div class="border-top border-white mb-3 w-75 ml-auto"></div>
+                                <blockquote>
+                                    <p class="lead">I have learnt to give not because I have much but because I know how it feels to have nothing</p>
+                                    <footer>Mother Teresa</footer>
+                                </blockquote>
+                                <a href="#" class="btn btn-primary btn-md">Learn More...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="w-100" src="img/children/3.jpg" alt="">
+            </div>
+            <div class="carousel-item">
+                <img class="w-100" src="img/children/5.jpg" alt="">
+                <div class="carousel-caption">
+                    <div class="container">
+                        <div class="row">
+                            <div class="d-none d-md-block col-md-9 col-lg-7 bg-white text-left text-dark">
+                                <h3>Athi River, Machakos County</h3>
+                                <div class="border-top border-dark my-1 w-75"></div>
+                                <p class="lead">Strong roots, Unlimited growth</p>
+                                <a href="#" class="btn btn-light btn-md">Learn More...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Carousel Content -->
+
+        <!-- Carousel Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1" class="active"></li>
+            <li data-target="#carousel" data-slide-to="2" class="active"></li>
+        </ol>
+
+        <!-- End Carousel Indicators -->
+
+    </div>
+    <!-- End Image Carousel -->
+
+</div> <!-- End Navigation Sticky ID Section -->
+
+<div id="social-media-sticky">
+
+    <!-- Start Social Jumbotron -->
+    <div class="jumbotron sticky-top">
+        <div class="container">
+            <div class="row justify-content-center text-center">
+                <div class="d-none d-xl-block col-xl-6 pt-1">
+                    <h3>Connect with us on social media!</h3>
+                </div>
+                <div class="col-sm-10 col-md-8 col-lg-7 col-xl-6 pt-1">
+                    <ul class="social">
+                        <li><a href="" target="_blank" title="Twitter"><i class="fab fa-twitter" style="color: #00aced"></i></a></li>
+                        <li><a href="" target="_blank" title="Facebook"><i class="fab fa-facebook" style="color: #3b5998"></i></a></li>
+                        <li><a href="" target="_blank" title="Instagram"><i class="fab fa-instagram" style="color: #3f729b"></i></a></li>
+                        <li><a href="" target="_blank" title="Youtube"><i class="fab fa-youtube" style="color: #c4302b"></i></a></li>
+                        <li><a href="" target="_blank" title="Gmail"><i class="fas fa-envelope" style="color: #0e76a8"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- End Social Jumbotron -->
+
+
+    <!-- Main Page Heading -->
+    <div class="col-12 text-center mt-5">
+        <h1 class="display-4 pt-4">PILLARS OF HOPE CHILDREN'S HOME</h1>
+        <div class="border-top border-dark w-50 mx-auto my-3"></div>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloribus, dolorum ducimus et ex in incidunt iusto maiores pariatur quae quas quidem totam ut veniam vitae. Debitis dignissimos nobis reprehenderit.</p>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloribus, dolorum ducimus et ex in incidunt iusto maiores pariatur quae quas quidem totam ut veniam vitae. Debitis dignissimos nobis reprehenderit.</p>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloribus, dolorum ducimus et ex in incidunt iusto maiores pariatur quae quas quidem totam ut veniam vitae. Debitis dignissimos nobis reprehenderit.</p>
+    </div>
+
+    <!-- Three Column Section -->
+    <div class="container">
+        <div class="row justify-content-center my-5 text-center">
+            <div class="col-md-6 col-lg-4 py-3">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-square fa-stack-2x"></i>
+                    <i class="fas fa-home fa-stack-1x fa-inverse" style="color: #026db6;"></i>
+                </span>
+                <p class="lead">Home is where the heart is</p>
+                <a href="" class="btn btn-outline-dark btn-md">Learn more...</a>
+            </div>
+            <div class="col-md-6 col-lg-4 py-3">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-square fa-stack-2x"></i>
+                    <i class="fas fa-hand-holding-heart fa-stack-1x fa-inverse" style="color: red;"></i>
+                </span>
+                <p class="lead">Give and you shall receive</p>
+                <a href="" class="btn btn-outline-dark btn-md">Learn more...</a>
+            </div>
+            <div class="col-md-6 col-lg-4 py-3">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-square fa-stack-2x"></i>
+                    <i class="fas fa-dove fa-stack-1x fa-inverse"></i>
+                </span>
+                <p class="lead">There is peace even in the storm</p>
+                <a href="" class="btn btn-outline-dark btn-md">Learn more...</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- End Three Column Section -->
+
+    <!-- border -->
+    <div class="border-top border-dark w-100 mx-auto my-3"></div>
+
+    <!-- Start Two Column Section -->
+    <div class="container mt-5">
+        <div class="row py-4">
+            <div class="col-lg-5 mb-4 my-lg-auto">
+                <h1 class="font-weight-bold mb-3">OUR WEBSITE</h1>
+                <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias asperiores aut beatae cupiditate est et expedita explicabo illo iste labore nesciunt provident quas quis repudiandae, suscipit tempore velit voluptatem!</p>
+                <a href="http://mywebsite.php" class="btn btn-outline-dark btn-lg" target="_blank">See My Website</a>
+            </div>
+            <div class="col-lg-7"><img src="img/tablet.png" alt="MY WEB LOGO" class="w-100"></div>
+        </div>
+    </div>
+
+    <!-- End Two Column Section -->
+
+</div> <!-- End Social Media Sticky -->
+
+
+<!-- Start Fixed Background IMG -->
+<div class="fixed-background">
+    <div class="fixed-wrap">
+        <div class="fixed">
+        </div>
+    </div>
+</div>
+<!-- End Fixed Background IMG -->
+
+
+<div id="text-button-sticky">
+
+    <!-- Start Bundle Pack Jumbotron -->
+    <div class="jumbotron sticky-top" style="margin-bottom: 2px;">
+        <div class="container">
+            <div class="row justify-content-center text-center">
+
+                <div class="d-none d-md-block col-md-8 col-xl-7 my-auto">
+                    <h4>Responsive bundle pack of complete websites!</h4>
+                </div>
+
+                <div class="col-md-4 col-xl-3 pt-1 pt-md-2 pb-2">
+                    <a href="https://w3newbie.com" target="_blank" class="btn btn-primary btn-md">Check out the websites</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- End Bundle Pack Jumbotron -->
+
+    <!-- Three Column Section -->
+
+    <!-- End Three Column Section -->
+
+    <!-- border -->
+    <div class="border-top border-dark w-100 mx-auto my-3"></div>
+
+</div> <!-- End Text Button Sticky -->
+
+
+<!-- Start Two Column Section -->
+
+<!-- End Two Column Section -->
+
+
+<!-- Start Website Designs Jumbotron --
+<div class="jumbotron py-5 mb-0 fixed-bottom">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-7 col-lg-8 col-xl-9 my-auto">
+                <h4>Looking for quality, easy-to-use mobile-friendly website designs?</h4>
+            </div>
+
+            <div class="col-md-5 col-lg-4 col-xl-3 pt-4 pt-md-0">
+                <a href="https://w3newbie.com" target="_blank" class="btn btn-primary btn-lg">Visit Us Today</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- End Website Designs Jumbotron -->
+
+
+<!-- Start Footer -->
+
+<!-- End Footer -->
+
+
+<!-- Start Socket -->
+
+<!-- End Socket -->
+
+<div class="space" style="height: 100vh;"></div>
+
+<!-- Script Source Files -->
+
+<!-- jQuery -->
+<script src="js/jQuery_3.5.1.js"></script>
+<!-- Bootstrap JS -->
+<script src="js/bootstrap.bundle.min.js"></script>
+<!-- Font Awesome -->
+<script src="js/all.min.js"></script>
+
+<!-- End Script Source Files -->
+</body>
+</html>
